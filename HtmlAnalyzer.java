a6dd23c03a1d957c66a6e3eeeb2aac26a03f20b9
José Henrique Oliveira de Carvalho <98109872+jose15000@users.noreply.github.com>
2023-03-27 17:46:43 -0300

    final version

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,97 +1,62 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class HtmlAnalyzer {
-
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Usage: java HtmlAnalyzer <url>");
+            System.err.println("Por favor, insira uma URL válida!");
             return;
         }
 
         String urlStr = args[0];
-        String html = getHtmlFromUrl(urlStr);
-
-        if (html == null) {
-            System.out.println("URL connection error");
-            return;
-        }
-
-        int maxDepth = 0;
-        String maxDepthText = null;
-
-        // Cria uma pilha para manter o rastreamento das tags abertas
-        Stack<String> openTags = new Stack<String>();
-
-        String[] lines = html.split("\\r?\\n"); // Divide o HTML em linhas
-
-        for (String line : lines) {
-            String trimmedLine = line.trim();
-
-            // Ignora linhas em branco
-            if (trimmedLine.isEmpty()) {
-                continue;
-            }
-
-            // Remove qualquer identação da linha
-            String unindentedLine = trimmedLine.replaceAll("^\\s+", "");
-
-            // Verifica se a linha é uma tag de abertura
-            if (unindentedLine.startsWith("<") && !unindentedLine.startsWith("</")) {
-                String tag = unindentedLine.substring(1, unindentedLine.indexOf(">"));
-                openTags.push(tag);
-            }
-
-            // Verifica se a linha é uma tag de fechamento
-            if (unindentedLine.startsWith("</")) {
-                openTags.pop();
-            }
-
-            // Verifica se a linha é um trecho de texto no nível máximo de profundidade
-            if (openTags.isEmpty()) {
-                int depth = getDepth(line);
-                if (depth > maxDepth) {
-                    maxDepth = depth;
-                    maxDepthText = trimmedLine;
-                }
-            }
-        }
-
-        if (maxDepthText != null) {
-            System.out.println(maxDepthText);
-        } else {
-            System.out.println("malformed HTML");
-        }
-    }
-
-    private static String getHtmlFromUrl(String urlStr) {
         try {
             URL url = new URL(urlStr);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            URLConnection connection = url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            StringBuilder sb = new StringBuilder();
+            Stack<String> tagStack = new Stack<>();
+            int deepestLevel = -1;
+            String deepestText = "";
+
             String line;
-            while ((line = in.readLine()) != null) {
-                sb.append(line);
-                sb.append("\n");
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                if (line.startsWith("</")) {
+                    try {
+                        tagStack.pop();
+                    } catch (EmptyStackException e) {
+                        System.err.println("malformed HTML");
+                        return;
+                    }
+                } else if (line.startsWith("<")) {
+                    tagStack.push(line);
+                } else {
+                    int currentLevel = tagStack.size();
+                    if (currentLevel > deepestLevel) {
+                        deepestLevel = currentLevel;
+                        deepestText = line;
+                    }
+                }
             }
-            in.close();
 
-            return sb.toString();
-        } catch (Exception e) {
-            return null;
+            if (tagStack.empty()) {
+                System.out.println(deepestText);
+            } else {
+                System.err.println("malformed HTML");
+            }
+        } catch (IOException e) {
+            System.err.println("URL connection error");
+            return;
         }
     }
-
-    private static int getDepth(String line) {
-        int depth = 0;
-        while (line.startsWith(" ")) {
-            depth++;
-            line = line.substring(1);
-        }
-        return depth;
-    }
-
 }
